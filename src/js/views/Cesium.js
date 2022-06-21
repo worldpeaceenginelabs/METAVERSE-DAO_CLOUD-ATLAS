da88ee19ce15f6b900cb846823cb913184c02557
Bo <67427045+worldpeaceenginelabs@users.noreply.github.com>
2022-06-21 23:09:39 +0200

    removed feed.js stuff from cesium.js

--- src/js/views/Cesium.js
+++ src/js/views/Cesium.js
@@ -1,14 +1,5 @@
-import { html } from 'htm/preact';
 import State from '../State';
-import FeedMessageForm from '../components/FeedMessageForm';
-import MessageFeed from '../components/MessageFeed';
-import Filters from '../components/Filters';
 import View from './View';
-import SubscribeHashtagButton from "../components/SubscribeHashtagButton";
-import Helmet from 'react-helmet';
-import HashtagList from '../components/HashtagList';
-import HashtagSubscriberList from '../components/HashtagSubscriberList';
-import OnboardingNotification from "../components/OnboardingNotification";
 
 class Feed extends View {
   constructor() {
@@ -47,49 +38,7 @@ class Feed extends View {
   }
 
   renderView() {
-    const s = this.state;
-    let path = this.props.index || 'msgs';
-    const hashtag = this.props.hashtag;
-    const hashtagText = `#${hashtag}`;
-    if (hashtag) {
-      path = `hashtags/${hashtag}`;
-    }
-    return html`
-      <div class="centered-container">
-        <div style="display:flex;flex-direction:row">
-          <div style="flex:3">
-            ${hashtag ? html`
-              <${Helmet}>
-                  <title>${hashtagText}</title>
-                  <meta property="og:title" content="${hashtagText} | Iris" />
-              <//>
-              <h3>${hashtagText} <span style="float:right"><${SubscribeHashtagButton} key=${hashtag} id=${hashtag} /></span></h3>
-            ` : ''}
-            ${s.searchTerm ? '' : html`
-              <${FeedMessageForm} key="form${path}" index=${path} class="hidden-xs" autofocus=${false}/>
-            `}
-            ${s.searchTerm ? html`<h2>Search results for "${s.searchTerm}"</h2>` : html`
-              <${OnboardingNotification} />
-            `}
-            ${!s.noFollows ? html`<${Filters}/>` : ''}
-            <${MessageFeed}
-                    scrollElement=${this.scrollElement.current}
-                    hashtag=${hashtag}
-                    filter=${s.searchTerm && (m => this.filter(m))}
-                    thumbnails=${this.props.thumbnails}
-                    key=${hashtag || this.props.index || 'feed'}
-                    group=${this.state.group}
-                    path=${path} />
-          </div>
-          ${this.props.index === 'media' ? '':html`
-            <div style="flex:1" class="hidden-xs">
-              <${HashtagList} />
-              ${this.props.hashtag ? html`<${HashtagSubscriberList} key=${this.props.hashtag} hashtag=${this.props.hashtag} />`:''}
-            </div>
-          `}
-        </div>
-      </div>
-    `;
+    
   }
 }
 
