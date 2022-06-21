ef193d54bbb2f2d8b396332b7b4be2842fe6322c
Bo <67427045+worldpeaceenginelabs@users.noreply.github.com>
2022-06-21 03:23:46 +0200

    updated

--- add-cesium.js
+++ add-cesium.js
@@ -1,11 +1,4 @@
-// This is how to add CesiumJS. I have two working snippets, one for React TS and one for Typesript
-
-// Maybe it helps to see them in action?
-// From https://github.com/worldpeaceenginelabs/PWA-JAMStack-Starter-Kit-with-Cesium (which is React TS)
-// From https://github.com/worldpeaceenginelabs/CLOUD-ATLAS-SVELTE, src/Cesium.svelte (which is Typescript)
-
-
-// 1. Insert this into template.hmtl above <!-- The main stylesheet -->
+// I already inserted this into template.hmtl above <!-- The main stylesheet -->
 
 // <!-- needed for cesium  -->
 // <link href="/build/Widgets/widgets.css" rel="stylesheet" />
@@ -13,30 +6,62 @@
 
 
 
-// 2. Clone /src/views/feed.js
-// Insert the Cesium React or Typescript snippet into the cloned feed.js and rename the file Cesium.ts
-
-// Then link Cesium.ts to Home in /src/js/components/Menu.js, and the existing feed.js entry in Menu.js
-// please rename to "Feed", placed under the Cesium Homebutton. (still "Home")
-
-// 3. add <CesiumMap /> component to Main.js
-// add Cesium.ts to router on Main.js (Menu.js "Home", now ./.)
-
-// 4. Then npm i cesium.
-
-// 5. npm install
-
-// Done (It should look like Mockup Starterkit in readme.md)
 
 
 
+// Typescript Snippet Cesium
+// no worries about Svelte, it acts just like normal Typescript
+// only difference: useEffect is replaced by onMount
+
+// I just having some issue with switching the js file to a ts file (something related to import / not a module something)
+
+// This goes into /src/js/views Cesium.js; rename to Cesium.ts
+import {onMount} from 'svelte'
+import {Viewer} from 'cesium'
+  import * as Cesium from 'cesium';
+	import {  } from 'cesium';
+	import '../node_modules/cesium/Build/Cesium/Widgets/widgets.css'
+
+
+  // avoid "window not declared"
+	if (typeof window !== "undefined"){
+		// browser code
+	}
+	
+  // cesium viewer
+  let viewer: Viewer;
+  // cesium basic settings
+  onMount(async () => { 
+		viewer = new Viewer('cesiumContainer', {
+    "animation": false,
+    "baseLayerPicker": false,
+    "fullscreenButton": false,
+    "vrButton": false,
+    "geocoder": false,
+    "homeButton": false,
+    "infoBox": false,
+    "sceneModePicker": false,
+    "selectionIndicator": false,
+    "timeline": false,
+    "navigationHelpButton": false,
+    "shouldAnimate": true
+    
+    //Use OpenStreetMaps
+    // imageryProvider : new Cesium.OpenStreetMapImageryProvider({
+    //    url : 'https://a.tile.openstreetmap.org/'
+    // })
+    
+		});
+	});
+	// cesium access token
+	Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkNTY0ZjMxYy1hZTdjLTRiMzQtYTc4Yi02NWQ5MzU4MWUxMjgiLCJpZCI6NDcwNzcsImlhdCI6MTYxNjg2MzYxOX0.V-4tUKhYM_XHdchqDu3MAAJPezusOzxMeimdYzCXd94';
 
 
 
 
-// This goes into Iris feed.js file. Its origin is a create react app with TS.
+  // Same snippet but React TS, maybe that helps?
 
-import React, { useEffect } from 'react';
+  import React, { useEffect } from 'react';
 
 import {
   createDefaultImageryProviderViewModels,
@@ -83,53 +108,3 @@ const CesiumMap: React.FunctionComponent = () => {
 });
 
 export default CesiumMap;
-
-
-
-
-
-
-
-
-
-// Typescript Version
-// This goes into Iris feed.js
-
-import {Viewer} from 'cesium'
-  import * as Cesium from 'cesium';
-	import {  } from 'cesium';
-	import '../node_modules/cesium/Build/Cesium/Widgets/widgets.css'
-
-
-  // avoid "window not declared"
-	if (typeof window !== "undefined"){
-		// browser code
-	}
-	
-  // cesium viewer
-  let viewer: Viewer;
-  // cesium basic settings
-  onMount(async () => { 
-		viewer = new Viewer('cesiumContainer', {
-    "animation": false,
-    "baseLayerPicker": false,
-    "fullscreenButton": false,
-    "vrButton": false,
-    "geocoder": false,
-    "homeButton": false,
-    "infoBox": false,
-    "sceneModePicker": false,
-    "selectionIndicator": false,
-    "timeline": false,
-    "navigationHelpButton": false,
-    "shouldAnimate": true
-    
-    //Use OpenStreetMaps
-    // imageryProvider : new Cesium.OpenStreetMapImageryProvider({
-    //    url : 'https://a.tile.openstreetmap.org/'
-    // })
-    
-		});
-	});
-	// cesium access token
-	Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkNTY0ZjMxYy1hZTdjLTRiMzQtYTc4Yi02NWQ5MzU4MWUxMjgiLCJpZCI6NDcwNzcsImlhdCI6MTYxNjg2MzYxOX0.V-4tUKhYM_XHdchqDu3MAAJPezusOzxMeimdYzCXd94';
