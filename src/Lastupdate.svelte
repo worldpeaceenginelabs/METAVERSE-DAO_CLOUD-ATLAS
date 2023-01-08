d58a5189a15784846bfc1148fd825ace315f82b2
Bo <67427045+worldpeaceenginelabs@users.noreply.github.com>
2023-01-08 05:46:52 +0100

    black bottom box round edges

--- src/Lastupdate.svelte
+++ src/Lastupdate.svelte
@@ -24,7 +24,10 @@
             text-align: center;
             color: white;
             padding: 1%;
-            font-size: large;        
+            font-size: large;
+            text-decoration: none;
+            
+            
         }
 
     h4  {
@@ -36,7 +39,48 @@
             color: white;
             background: black;
             padding: 1%;
-            font-size: large;        
+            font-size: large;
+            
+            /* Safari 5, Chrome support border-radius without vendor prefix.
+		* FF 3.0/3.5/3.6, Mobile Safari 4.0.4 require vendor prefix.
+		* No support in Safari 3/4, IE 6/7/8, Opera 10.0.
+		*/
+		-moz-border-radius: 10px;
+		-webkit-border-radius: 10px;
+		border-radius: 10px;
+		
+		/* Chrome, FF 4.0 support box-shadow without vendor prefix.
+		* Safari 3/4/5 and FF 3.5/3.6 require vendor prefix.
+		* No support in FF 3.0, IE 6/7/8, Opera 10.0, iPhone 3.
+		* change the offsets, blur and color to suit your design.
+		*/
+		-moz-box-shadow: 2px 2px 3px #666;
+		-webkit-box-shadow: 2px 2px 3px #666;
+		box-shadow: 2px 2px 3px #666;
+		
+		/* using a bigger font for demo purposes so the box isn't too small */
+		font-size: 20px;
+		
+		/* with a big radius/font there needs to be padding left and right
+		* otherwise the text is too close to the radius.
+		* on a smaller radius/font it may not be necessary
+		*/
+		padding: 4px 7px;
+		
+		/* only needed for webkit browsers which show a rectangular outline;
+		* others do not do outline when radius used.
+		* android browser still displays a big outline
+		*/
+		outline: 0;
+
+		/* this is needed for iOS devices otherwise a shadow/line appears at the
+		* top of the input. depending on the ratio of radius to height it will
+		* go all the way across the full width of the input and look really messy.
+		* ensure the radius is no more than half the full height of the input, 
+		* and the following is set, and everything will render well in iOS.
+		*/
+		-webkit-appearance: none;
+
         }
 
     div {
@@ -46,6 +90,9 @@
             display: flex;
             flex-flow: row;
             justify-content: center;
-            align-items: center;  
+            align-items: center;
+            
+            
+            
         }
     </style>
