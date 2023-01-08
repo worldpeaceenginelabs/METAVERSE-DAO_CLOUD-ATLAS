1483c4a05cab3e22fef4a5f51f508632ab175c32
Bo <67427045+worldpeaceenginelabs@users.noreply.github.com>
2023-01-08 06:59:34 +0100

    css bottom infobox

--- src/Lastupdate.svelte
+++ src/Lastupdate.svelte
@@ -1,71 +1,47 @@
 <script lang="ts">
 </script>
 
-<div>
-
+<div class="main_div" >
+<div class="sub_div">
     <h4>
         Want to support decentralization?<br><br>
         Get involved...<br>
         <a href="https://github.com/worldpeaceenginelabs/METAVERSE-DAO" target="blank">@Github</a>
         <a href="https://twitter.com/peace_engine" target="blank">@Twitter</a>
-        <a href="https://www.youtube.com/@worldpeaceenginelabs3955/videos" target="_blank">@YouTube</a>
+        <a href="https://www.youtube.com/@worldpeaceenginelabs3955/videos" target="_blank" rel="">@YouTube</a>
         
     </h4>
-
+</div>
 </div>
 
 
 <style>
     a   {
-            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-            'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
-            -webkit-font-smoothing: antialiased;
-            -moz-osx-font-smoothing: grayscale;
-            text-align: center;
-            color: white;
-            padding: 1%;
-            font-size: large;
-            text-decoration: none;
-            
-            
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+        'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: white;
+        padding: 1%;        
+        text-decoration: none;           
         }
 
     h4  {
-            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-            'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
-            -webkit-font-smoothing: antialiased;
-            -moz-osx-font-smoothing: grayscale;
-            text-align: center;
-            color: white;
-            background: black;
-            padding: 1%;
-            font-size: large;
-            
-            /* Safari 5, Chrome support border-radius without vendor prefix.
-		* FF 3.0/3.5/3.6, Mobile Safari 4.0.4 require vendor prefix.
-		* No support in Safari 3/4, IE 6/7/8, Opera 10.0.
-		*/
-		-moz-border-radius: 10px;
-		-webkit-border-radius: 10px;
-		border-radius: 10px;
-		
-		/* Chrome, FF 4.0 support box-shadow without vendor prefix.
-		* Safari 3/4/5 and FF 3.5/3.6 require vendor prefix.
-		* No support in FF 3.0, IE 6/7/8, Opera 10.0, iPhone 3.
-		* change the offsets, blur and color to suit your design.
-		*/
-		-moz-box-shadow: 2px 2px 3px #666;
-		-webkit-box-shadow: 2px 2px 3px #666;
-		box-shadow: 2px 2px 3px #666;
-		
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+        'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: white;
+        		
 		/* using a bigger font for demo purposes so the box isn't too small */
-		font-size: 20px;
 		
 		/* with a big radius/font there needs to be padding left and right
 		* otherwise the text is too close to the radius.
 		* on a smaller radius/font it may not be necessary
 		*/
-		padding: 4px 7px;
+		padding: 0px 20px;
 		
 		/* only needed for webkit browsers which show a rectangular outline;
 		* others do not do outline when radius used.
@@ -83,16 +59,37 @@
 
         }
 
-    div {
-            position: relative;
-            height: 100%;
-            width: 100%;
-            display: flex;
-            flex-flow: row;
-            justify-content: center;
-            align-items: center;
-            
-            
-            
-        }
+    .main_div {
+        position: absolute;
+        bottom: 5px;                  
+        width: 100%;                    
+        display: flex;
+        flex-direction: column;    
+    
+    }
+
+    .sub_div {
+        
+        background: black;
+        margin: auto;
+        
+
+        /* Safari 5, Chrome support border-radius without vendor prefix.
+		* FF 3.0/3.5/3.6, Mobile Safari 4.0.4 require vendor prefix.
+		* No support in Safari 3/4, IE 6/7/8, Opera 10.0.
+		*/
+		-moz-border-radius: 10px;
+		-webkit-border-radius: 10px;
+		border-radius: 10px;
+
+        /* Chrome, FF 4.0 support box-shadow without vendor prefix.
+		* Safari 3/4/5 and FF 3.5/3.6 require vendor prefix.
+		* No support in FF 3.0, IE 6/7/8, Opera 10.0, iPhone 3.
+		* change the offsets, blur and color to suit your design.
+		*/
+		-moz-box-shadow: 2px 2px 3px #666;
+		-webkit-box-shadow: 2px 2px 3px #666;
+		box-shadow: 2px 2px 3px #666;
+
+        }   
     </style>
